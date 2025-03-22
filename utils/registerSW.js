062a9db039e1cbbde33a368fad325f114a340efb
Surendra Singh <surendrasinghc80@gmail.com>
2025-03-23 00:59:26 +0530

    major canges in service worker

---
+++ utils/registerSW.js
@@ -0,0 +1,14 @@
+export function register() {
+  if ("serviceWorker" in navigator && process.env.NODE_ENV === "production") {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker
+        .register("service-worker.js")
+        .then((registration) => {
+          console.log("SW registered:", registration);
+        })
+        .catch((error) => {
+          console.log("SW registration failed:", error);
+        });
+    });
+  }
+}
